8e2f378bc522e78f8972a95e585fd5c75d3afb31
oOStroudyOo <111181956+oOStroudyOo@users.noreply.github.com>
2023-05-15 10:28:48 +0100

    Update homepage-install.sh

--- homepage-install.sh
+++ homepage-install.sh
@@ -6,8 +6,8 @@ services:
     container_name: homepage
     volumes:
       - ./config:/config
-      - ./media/wdnas4tb
-      - ./media/wdnas6tb
+      - ./media/wdnas4tb:/media/wdnas4tb
+      - ./media/wdnas6tb:/media/wdnas6tb
     ports:
       - 3000:3000
     restart: unless-stopped' >> ~/dockers/homepage/docker-compose.yml && cd ~/dockers/homepage && docker pull ghcr.io/benphelps/homepage:latest && docker compose up -d
