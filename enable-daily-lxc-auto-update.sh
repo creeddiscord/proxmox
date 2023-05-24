8e1875826b42bf244f57acba3293c04d9812aa5c
oOStroudyOo <111181956+oOStroudyOo@users.noreply.github.com>
2023-05-24 13:15:12 +0100

    Update enable-daily-lxc-auto-update.sh

--- enable-daily-lxc-auto-update.sh
+++ enable-daily-lxc-auto-update.sh
@@ -1,1 +1,1 @@
-cat '0 0     * * *   root    apt-get update && apt-get upgrade -y >/dev/null 2>&1' | tee /etc/crontab
+echo '0 0     * * *   root    apt-get update && apt-get upgrade -y >/dev/null 2>&1' >> /etc/crontab
