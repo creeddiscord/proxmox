100e1ca229eb268a6c560fc112d3a748b69f380e
oOStroudyOo <111181956+oOStroudyOo@users.noreply.github.com>
2023-05-24 13:41:55 +0100

    Changed to dist-upgrade

--- enable-daily-lxc-auto-update.sh
+++ enable-daily-lxc-auto-update.sh
@@ -1,1 +1,1 @@
-echo '0 0     * * *   root    apt-get update && apt-get upgrade -y >/dev/null 2>&1' | tee -a /etc/crontab
+echo '0 0     * * *   root    sudo apt-get update && sudo apt-get dist-upgrade -y >/dev/null 2>&1' | tee -a /etc/crontab
