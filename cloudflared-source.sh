ef2d2173e02092a12a163a0906410f8db97faf38
oOStroudyOo <111181956+oOStroudyOo@users.noreply.github.com>
2023-06-27 03:02:19 +0100

    Updated to PVE 8

--- cloudflared-source.sh
+++ cloudflared-source.sh
@@ -1,3 +1,3 @@
 mkdir -p --mode=0755 /usr/share/keyrings
 curl -fsSL https://pkg.cloudflare.com/cloudflare-main.gpg | tee /usr/share/keyrings/cloudflare-main.gpg >/dev/null
-echo 'deb [signed-by=/usr/share/keyrings/cloudflare-main.gpg] https://pkg.cloudflare.com/cloudflared bullseye main' | tee /etc/apt/sources.list.d/cloudflared.list >/dev/null
+echo 'deb [signed-by=/usr/share/keyrings/cloudflare-main.gpg] https://pkg.cloudflare.com/cloudflared bookworm main' | tee /etc/apt/sources.list.d/cloudflared.list >/dev/null
