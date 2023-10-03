a4e940b31097503c95fbf210d80958fa67bebc3a
oOStroudyOo <111181956+oOStroudyOo@users.noreply.github.com>
2023-10-03 01:38:19 +0100

    Create watchtower-install.sh

---
+++ watchtower-install.sh
@@ -0,0 +1,15 @@
+cd ~ && mkdir -p ~/dockers/watchtower/ && echo '---
+version: "3"
+
+services:
+  watchtower:
+    container_name: watchtower
+    restart: always
+    environment:
+      WATCHTOWER_SCHEDULE: 0 0 * * * *
+      TZ: Europe/London
+      WATCHTOWER_CLEANUP: "true"
+      WATCHTOWER_DEBUG: "true"
+    image: containrrr/watchtower:latest
+    volumes:
+      - /var/run/docker.sock:/var/run/docker.sock' > ~/dockers/watchtower/docker-compose.yml && cd ~/dockers/watchtower && docker compose up -d
