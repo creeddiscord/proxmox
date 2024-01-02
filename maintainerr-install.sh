90ebb8f21adcfa879ef3b59b89fa380876844f00
oOStroudyOo <111181956+oOStroudyOo@users.noreply.github.com>
2024-01-02 13:26:44 +0000

    Create maintainerr-install.sh

---
+++ maintainerr-install.sh
@@ -0,0 +1,13 @@
+cd ~ && mkdir -p ~/dockers/maintainerr/ && echo 'version: '3'
+
+services:
+  maintainerr:
+    image: jorenn92/maintainerr:latest
+    container_name: maintainerr
+    volumes:
+      - ./data:/opt/data
+    environment:
+      - TZ=Europe/London
+    ports:
+      - 8154:8154
+    restart: unless-stopped' > ~/dockers/maintainerr/docker-compose.yml && cd ~/dockers/maintainerr && docker compose up -d
