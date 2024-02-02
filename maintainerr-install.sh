6a519b8ce2b2480349e40f5346a3aa34661ace1a
oOStroudyOo <111181956+oOStroudyOo@users.noreply.github.com>
2024-02-02 21:45:28 +0000

    Maintainerr v2

--- maintainerr-install.sh
+++ maintainerr-install.sh
@@ -1,15 +1,18 @@
 cd ~ && mkdir -p ~/dockers/maintainerr/ && echo '---
-version: "3"
+version: '3'
 
 services:
-  maintainerr:
-    image: ghcr.io/jorenn92/maintainerr:latest
-    container_name: maintainerr
-    user: 1000:1000
-    volumes:
-      - ./data:/opt/data
-    environment:
-      - TZ=Europe/London
-    ports:
-      - 6246:6246
-    restart: unless-stopped' > ~/dockers/maintainerr/docker-compose.yml && cd ~/dockers/maintainerr && docker compose up -d
+    maintainerr:
+        image: ghcr.io/jorenn92/maintainerr:latest # or jorenn92/maintainerr:latest
+        container_name: maintainerr
+        user: 1000:1000
+        volumes:
+          - type: bind
+            source: ./data
+            target: /opt/data
+        environment:
+          - TZ=Europe/London
+#      - DEBUG=true # uncomment to enable debug logs
+        ports:
+          - 6246:6246
+        restart: unless-stopped' > ~/dockers/maintainerr/docker-compose.yml && cd ~/dockers/maintainerr && docker compose up -d
