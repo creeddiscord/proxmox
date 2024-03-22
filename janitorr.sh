f98394ca57e27c5334c2da2e635dea97a0ac62d6
oOStroudyOo <111181956+oOStroudyOo@users.noreply.github.com>
2024-03-22 17:37:54 +0000

    Create janitorr.sh

---
+++ janitorr.sh
@@ -0,0 +1,13 @@
+cd ~ && mkdir -p ~/dockers/janitorr/ && echo '---
+version: '3'
+
+services:
+  janitorr:
+    container_name: janitorr
+    image: schaka/janitorr:develop
+    ports:
+      - 8978:8978 # Technically, we don't publish any endpoints, so this isn't strictly required
+    volumes:
+      - /appdata/janitorr/config:/config 
+      - /share_media:/data
+    restart: unless-stopped' > ~/dockers/janitorr/docker-compose.yml && cd ~/dockers/janitorr && docker compose up -d
