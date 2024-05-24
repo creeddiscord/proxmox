650805f57f27c6c9354814585982e8aea4538bf2
oOStroudyOo <111181956+oOStroudyOo@users.noreply.github.com>
2024-05-24 14:57:30 +0100

    Create debian-update.sh

---
+++ debian-update.sh
@@ -0,0 +1,6 @@
+cat <<EOF >/etc/apt/sources.list
+deb http://ftp.debian.org/debian sid main contrib
+deb http://ftp.debian.org/debian trixie-updates main contrib
+deb http://security.debian.org/debian-security trixie-security main contrib
+EOF
+apt-get update && apt-get upgrade -y && apt-get autoremove -y
