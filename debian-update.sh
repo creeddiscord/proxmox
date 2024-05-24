c1ec98523b6bb656f04f009e7af7d09f462f0dae
oOStroudyOo <111181956+oOStroudyOo@users.noreply.github.com>
2024-05-24 14:59:35 +0100

    Update debian-update.sh

--- debian-update.sh
+++ debian-update.sh
@@ -3,4 +3,4 @@ deb http://ftp.debian.org/debian sid main contrib
 deb http://ftp.debian.org/debian trixie-updates main contrib
 deb http://security.debian.org/debian-security trixie-security main contrib
 EOF
-apt-get update && apt-get upgrade -y && apt-get autoremove -y
+apt-get update && apt-get upgrade -y && apt-get autoremove -y && reboot
